7fa8d7fad2a40b93e9bf6a9da748361583d95d15
thienth32 <thienth32@gmail.com>
2020-09-16 08:14:29 +0700

    Update index.php

--- lesson1/index.php
+++ lesson1/index.php
@@ -7,15 +7,23 @@ require_once './db.php';
 $lenhSql = "select * from products";
 $products = truy_van($lenhSql);
 
-echo "<pre>";
-var_dump($products);die;
-
 // 4. tạo vòng lặp dựa vào kết quả lấy được để hiển thị danh sách (table)
-
-
-
-
-
-
-
 ?>
+
+<table>
+    <thead>
+        <th>ID</th>
+        <th>Tên sp</th>
+        <th>Giá</th>
+    </thead>
+    <tbody>
+        <?php foreach($products as $pro): ?>
+        <tr>
+            <td><?php echo $pro['id'];?></td>
+            <td><?php echo $pro['name'];?></td>
+            <td><?php echo $pro['price'];?></td>
+        </tr>
+
+        <?php endforeach ?>
+    </tbody>
+</table>
